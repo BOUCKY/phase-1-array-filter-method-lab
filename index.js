c5e74e83f42bd7230f12c589e777a1926e55d155
Alexis <alexisbou16@gmail.com>
2023-05-23 12:58:33 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching(driverNames, string){
+    const nameMatches = driverNames.filter(name => name.toLowerCase() === string.toLowerCase())
+    return nameMatches;
+}
+
+function fuzzyMatch(driverNames, string){
+    const firstLetterMatches = driverNames.filter(letter => letter[0] === string[0])
+    return firstLetterMatches;
+}
+
+function matchName(driverNames, string){
+    const matchingNames = driverNames.filter(driver => driver.name === string)
+    return matchingNames; 
+}
